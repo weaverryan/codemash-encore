229979ce742f4a0cda0aac8377fd4a70013a8dfa
Ryan Weaver <ryan@thatsquality.com>
2019-01-09 11:19:08 -0500

    Adding a "dummy" index.php file

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This is a dummy file to represent the main file that boots your
+ * application. I've put HTML below just to show how Encore would
+ * work (e.g. what the script/link tags look like) in a simple way.
+ * Normally you would add the script/link tags onto your page
+ * however you normally do in your framework or application.
+ */
+?>
+<DOCTYPE! html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Example page to show off Webpack Encore</title>
+</head>
+<body>
+    <section>
+        <h1>Hello Webpack Encore!</h1>
+    </section>
+</body>
+</html>
